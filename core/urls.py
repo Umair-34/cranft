d58aea5c2307d5be4e686834a97b84a2825e6cb8
muhammad <umair.dev34k@gmail.com>
2021-08-26 19:56:35 +0500

    blog

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index, BlogList
+from .views import index, BlogList, BlogDetail
+
+app_name = 'core'
 
 urlpatterns = [
     path('',index, name='home'),
-    path('blog-list',BlogList, name='blog-list'),
+    path('blog-list',BlogList, name='bloglist'),
+    path('blog-detail/<int:id>/',BlogDetail, name='blogdetail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 
 def index(request):
@@ -7,4 +8,15 @@ def index(request):
 
 
 def BlogList(request):
-    return render(request, 'core/blog_list.html')
+    queryset = Blog.objects.all()
+    context ={
+        'obj' : queryset,
+    }
+    return render(request, 'core/blog_list.html', context)
+
+def BlogDetail(request, id):
+    queryset = Blog.objects.get(id=id)
+    context ={
+        'obj' : queryset,
+    }
+    return render(request, 'core/blog_detail.html', context)
